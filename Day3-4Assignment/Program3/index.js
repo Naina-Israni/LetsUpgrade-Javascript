43badd5ad5447a7bcba283641eaace22f4714435
Naina-Israni <70740388+Naina-Israni@users.noreply.github.com>
2020-09-11 12:15:17 +0530

    Add files via upload

---
+++ Day3-4Assignment/Program3/index.js
@@ -0,0 +1,33 @@
+let array = [
+    {
+      name: "Alice",
+      age: 23,
+      country: "India",
+      hobbies: ["Cooking", "Painting", "Coding"],
+    },
+    {
+      name: "Mark",
+      age: 33,
+      country: "America",
+      hobbies: ["Playing Guitar", "Cricket", "Coding"],
+    },
+    {
+      name: "Martin",
+      age: 30,
+      country: "Korea",
+      hobbies: ["Scoccer", "Hacking", "Riding"],
+    },
+    {
+      name: "Julia",
+      age: 25,
+      country: "Russia",
+      hobbies: ["Dancing", "Gardening", "Riding"],
+    },
+  ];
+  
+
+function arrayOfObjects(){
+console.log(array);
+}
+
+arrayOfObjects();
